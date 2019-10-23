43d492002089b3beadab1d47643748521740a9f7
ynguessan <ynguessan.timwi@gmail.com>
2019-10-23 15:24:49 +0200

    set up ball view

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,44 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.blue,
+          appBar: AppBar(
+            title: Text('Ask Me Anything'),
+            backgroundColor: Colors.blue.shade900,
+          ),
+          body: BallView(),
+        ),
       ),
     );
+
+class BallView extends StatefulWidget {
+  @override
+  _BallViewState createState() => _BallViewState();
+}
+
+class _BallViewState extends State<BallView> {
+  int _ballNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Expanded(
+          child: FlatButton(
+            onPressed: () {
+              setState(() {
+                _ballNumber = Random().nextInt(5) + 1;
+              });
+            },
+            child: Image.asset('images/ball$_ballNumber.png'),
+          ),
+        ),
+      ],
+    );
+  }
+}
